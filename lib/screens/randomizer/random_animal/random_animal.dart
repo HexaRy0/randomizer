5f8d746f0f6280eeee1a068b53f59c153ccb74df
Merza Bolivar <merza.bolivar@gmail.com>
2023-07-20 10:58:50 +0700

    feat: add copy function on random animal generator

--- lib/screens/randomizer/random_animal/random_animal.dart
+++ lib/screens/randomizer/random_animal/random_animal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:randomizer/providers/random_animal_provider.dart';
@@ -53,32 +54,99 @@ class _RandomAnimalScreenState extends ConsumerState<RandomAnimalScreen> {
                 child: Card(
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                      child: SingleChildScrollView(
-                        child: randomAnimal.isEmpty
-                            ? const Text("Press the button to generate animal")
-                            : ListView.builder(
-                                shrinkWrap: true,
-                                primary: false,
-                                itemCount: randomAnimal.length,
-                                itemBuilder: (context, index) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(4),
-                                    child: Card(
-                                      color: Theme.of(context).colorScheme.secondaryContainer,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(12),
-                                        child: Text(
-                                          randomAnimal[index],
-                                          textAlign: TextAlign.center,
-                                          style: Theme.of(context).textTheme.titleLarge,
+                    child: Stack(
+                      children: [
+                        Center(
+                          child: SingleChildScrollView(
+                            child: randomAnimal.isEmpty
+                                ? const Text("Press the button to generate word")
+                                : ListView.builder(
+                                    shrinkWrap: true,
+                                    primary: false,
+                                    itemCount: randomAnimal.length,
+                                    itemBuilder: (context, index) {
+                                      return Padding(
+                                        padding: const EdgeInsets.all(4),
+                                        child: Card(
+                                          color: Theme.of(context).colorScheme.secondaryContainer,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(12),
+                                            child: Stack(
+                                              children: [
+                                                Container(
+                                                  padding: const EdgeInsets.all(8),
+                                                  width: double.infinity,
+                                                  child: Text(
+                                                    randomAnimal[index],
+                                                    textAlign: TextAlign.center,
+                                                    style: Theme.of(context).textTheme.titleLarge,
+                                                  ),
+                                                ),
+                                                Positioned(
+                                                  right: 0,
+                                                  child: IconButton.filled(
+                                                    style: ButtonStyle(
+                                                      backgroundColor:
+                                                          MaterialStateProperty.all<Color>(
+                                                        Theme.of(context)
+                                                            .colorScheme
+                                                            .primaryContainer,
+                                                      ),
+                                                      foregroundColor:
+                                                          MaterialStateProperty.all<Color>(
+                                                        Theme.of(context)
+                                                            .colorScheme
+                                                            .onPrimaryContainer,
+                                                      ),
+                                                    ),
+                                                    icon: const Icon(Icons.copy),
+                                                    onPressed: () async {
+                                                      await Clipboard.setData(
+                                                        ClipboardData(
+                                                          text: randomAnimal[index].toString(),
+                                                        ),
+                                                      );
+                                                    },
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ),
                                         ),
+                                      );
+                                    },
+                                  ),
+                          ),
+                        ),
+                        randomAnimal.length <= 1
+                            ? Container()
+                            : Positioned(
+                                bottom: 0,
+                                left: 0,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: ElevatedButton.icon(
+                                    label: const Text("Copy All"),
+                                    style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all<Color>(
+                                        Theme.of(context).colorScheme.primaryContainer,
+                                      ),
+                                      foregroundColor: MaterialStateProperty.all<Color>(
+                                        Theme.of(context).colorScheme.onPrimaryContainer,
                                       ),
                                     ),
-                                  );
-                                },
+                                    icon: const Icon(Icons.copy),
+                                    onPressed: () async {
+                                      await Clipboard.setData(
+                                        ClipboardData(
+                                          text: randomAnimal.join(", ").toString(),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
                               ),
-                      ),
+                      ],
                     ),
                   ),
                 ),
